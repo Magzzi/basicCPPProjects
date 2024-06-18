937746c35da0aa39824a1d52f94ad6827020b6ac
Magzzi <168576629+Magzzi@users.noreply.github.com>
2024-06-18 16:58:43 +0800

    finalize

---
+++ CBC/cbc.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+#include <limits>
+#include <windows.h>
+
+using namespace std;
+
+class Calculator{
+    private:
+        int res = 0;
+        
+        
+    public:
+
+        int addition(int num);
+        int subtraction(int num);
+        int multiplication(int num);
+        int division(int num);
+        void getResult();
+};
+
+int Calculator::addition(int num){
+    res += num;
+    getResult();
+    return res;
+}
+
+int Calculator::subtraction(int num){
+    res -= num;
+    getResult();
+    return res;
+}
+
+int Calculator::multiplication(int num){
+    res *= num;
+    getResult();
+    return res;
+}
+
+int Calculator::division(int num){
+    if (num != 0) {
+        res /= num;
+    } else {
+        cout << "Error: Division by zero." << endl;
+    }
+    getResult();
+    return res;
+}
+
+void Calculator::getResult(){
+    cout << "Result: " << res << endl;
+}
+
+
+int main()
+{
+    Calculator c;
+    
+    char op;
+    
+    int n;
+    
+    while (true){
+        system ("cls");
+        c.getResult();
+        cout << "Enter a number: ";
+        if (!(cin >> n)) {
+            cout << "Invalid input. Please enter a valid number." << endl;
+            cin.clear(); // Clear error flag
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
+            continue;
+        }
+        
+        cout << "Enter an operation (+, - , *, /, =): ";
+        cin >> op;
+        switch(op){
+            case '+':
+                c.addition(n);
+                break;
+            case '-':
+                c.subtraction(n);
+                break;
+            case '*':
+                c.multiplication(n);
+                break;
+            case '/':
+                c.division(n);
+                break;
+            case '=':
+                c.getResult();
+                return 0;
+            default:
+                cout << "Invalid Operation";
+                break;
+        }
+        cout << "\n\n---------------------------------------------\n\n";
+    }
+
+    return 0;
+}
